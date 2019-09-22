aae977d3e9a8c479a060119ce5538d0337d6ece2
Andrew Capp <acapp909@gmail.com>
2019-09-22 20:53:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,7 +6,7 @@ def find_element_index(array, value_to_find)
   # if value_to_find = array.index[value]
    #  return array.index[value]
    # end
-  if value_to_find = array.index (value1)
+  if array.[value1] = value_to_find
     return value1
   end
     puts value1
