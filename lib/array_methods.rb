f04dcc3b9fd85307e353fc4bc4c9d2fa4e13fd29
Andrew Capp <acapp909@gmail.com>
2019-09-22 20:33:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ array1 = [1,5,7,9,10]
 
 def find_element_index(array, value_to_find)
   # Add your solution here
-  array.length.times do |index|
+  array.times.length do |index|
   # if value_to_find = array.index[value]
    #  return array.index[value]
    # end
