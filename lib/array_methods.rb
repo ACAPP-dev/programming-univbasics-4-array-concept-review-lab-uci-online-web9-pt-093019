c68c896a623fa49851cec0ee8163ef3039708c4b
Andrew Capp <acapp909@gmail.com>
2019-09-22 20:38:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,7 +6,7 @@ def find_element_index(array, value_to_find)
   # if value_to_find = array.index[value]
    #  return array.index[value]
    # end
-  puts array.value1
+  puts array.index
 end
 end
 
